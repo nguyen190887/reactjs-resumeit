825abfc51bd156a6edf6bc34d0fb595aa5d16546
tri.nguyen <tri.nguyen@kbb.com>
2017-07-20 17:36:14 +0700

    Add temperature component - demo lifting state up

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,9 +3,9 @@ import {render} from 'react-dom';
 import ActionLink from './components/ActionLink.jsx';
 import Toggle from './components/ToggleLink.jsx';
 import LoggingButton from './components/LoggingButton.jsx';
-
 import LoginControl from './components/greeting/LoginControl.jsx';
 import NameForm from './components/forms/NameForm.jsx';
+import Calculator from './components/forms/TemperatureCalculator.jsx';
 
 class App extends React.Component {
   render () {
@@ -21,6 +21,8 @@ class App extends React.Component {
         </div>
         <hr />
         <NameForm />
+        <hr />
+        <Calculator />
       </div>
     );
   }
